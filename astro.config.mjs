d942945db2efce71f7bad9266b1fc1b0ae3f54ae
Gabriel Soares <g4brieloliveirasoares@gmail.com>
2026-03-12 23:15:56 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,15 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from "@astrojs/node";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://soareso.com',
   integrations: [mdx(), sitemap()],
   output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
